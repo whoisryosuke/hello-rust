fbb0117667f6403b9404f1ec177a10d47ddecceb
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-30 13:13:07 -0700

    :zap: read from text file and print contents

--- examples/cli-tutorial/main.rs
+++ examples/cli-tutorial/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,6 +9,11 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents =
+        fs::read_to_string(file_path).expect("Couldn't read the file from the path provided");
+
+    println!("With text:\n{contents}");
 
     // Run code using `cargo run --example cli-tutorial the README.md`
     // would ideally search for text "the" in the file "README.md"
