9a1062850234482489f9dfc3ba06f68c95a77629
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-30 16:07:29 -0700

    :zap: complete the CLI search + docs

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,25 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Question mark (?) at end lets us return any errors from `fs`
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    // Run the search function
+    // And loop through the results to print out each "match"
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     // If we got the file, return Ok type an empty tuple (which functions usually return?)
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // Storage for lines of strings that match our search term
     let mut results = Vec::new();
 
+    // Go through each "line" inside the body of text
     for line in contents.lines() {
+        // Check if the line "contains" the search term
         if line.contains(query) {
+            // Add it to the results vector
             results.push(line);
         }
     }
