4d96e9b948b74e61e51965575fd8e3c77c4fa6f9
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-30 14:29:46 -0700

    :hammer: better errors with result ok + err

--- examples/cli-tutorial/main.rs
+++ examples/cli-tutorial/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // Parse CLI arguments
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing args: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -26,14 +30,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         // Check if user passed enough args into CLI command
         if args.len() < 3 {
-            panic!("Not enough arguments. Try `cargo run search-term path/to/file.md`")
+            return Err("Not enough arguments. Try `cargo run search-term path/to/file.md`");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
