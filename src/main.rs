dfba10eaa2937f0838d303560d1b3c0819a2de3d
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-10-05 00:35:36 -0700

    :sparkles: threads with messaging

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
-use std::env;
-use std::process;
-
-use hello_rust::Config;
+use std::sync::mpsc;
+use std::thread;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let (tx, rx) = mpsc::channel();
 
-    // Parse CLI arguments
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {err}");
-        process::exit(1);
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    // Read from user's file
-    // Run the function (and does anything inside)
-    // then checks if we return an error
-    if let Err(e) = hello_rust::run(config) {
-        // Got an error? Let the user know and crash app gracefully
-        println!("Application error!: {e}");
-        process::exit(1);
-    }
-
-    // Run code using `cargo run --example cli-tutorial the README.md`
-    // would ideally search for text "the" in the file "README.md"
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }
